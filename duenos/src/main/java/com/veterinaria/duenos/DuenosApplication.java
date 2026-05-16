81ae149419f04c8658256d0a2ac43551b2f16ef1
alexvicuna <alexandra.vic00@gmail.com>
2026-05-16 19:30:52 -0400

    feat: estructura inicial microservicio duenos

---
+++ duenos/src/main/java/com/veterinaria/duenos/DuenosApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.duenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuenosApplication.class, args);
+	}
+
+}
---
+++ duenos/src/test/java/com/veterinaria/duenos/DuenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.duenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
